bae09ace4d9bbe719b1f69cfb50636174d2de5f1
Senkhosi <senkhxsi@gmail.com>
2026-01-08 10:21:56 +0200

    add daily analysis runner script and SARAO GNSS measurements

---
+++ BMAC/daily_run.sh
@@ -0,0 +1,66 @@
+# Master script to compute profile statistics, generate am models, and compute
+# spectra.
+
+# Site name, date range, and path to directory containing the  data files.
+export SITE=BMAC
+export SITE_LABEL='BMAC'
+export SITE_ALT=3000
+# export DATERANGE=2009-2022
+export SITE_DIR=~/projects/ngeht_site_characterisation/${SITE}
+export DATADIR=${SITE_DIR}/march_24
+export SCRIPTS_DIR=~/projects/ngeht_site_characterisation/scripts/daily
+# export VENV=~/projects/ngeht_site_characterisation/.venv
+
+# Site coordinates, and bracketing MERRA-2 grid coordinates
+export SITE_LAT=-30.648081
+export SITE_LONG=27.935199
+
+export MERRA_LAT0=-31
+export MERRA_LAT1=-30.5
+export MERRA_LONG0=27.5
+export MERRA_LONG1=28.125
+
+# The surface pressure at the MERRA-2 grid points may not match the nominal
+# site surface pressure.  The following constants control truncation
+# of the MERRA-2 profiles to some point above the surface (such that all
+# levels have valid data), and interpolation or extrapolation to the
+# nominal surface pressure.
+
+# include levels above this pressure level:
+export PTRUNC=900. # truncation point [mbar] of MERRA-2 profiles
+
+# Frequency range [GHz] for am models
+export F_MIN=80.
+export F_MAX=400.
+# Frequency interval [MHz]
+export DF=500.
+
+# Compute profile statistics.
+export OUTDIR_PROFILES=${SITE_DIR}/daily_profile_stats
+if [ ! -d $OUTDIR_PROFILES ]; then
+    mkdir $OUTDIR_PROFILES
+fi
+echo computing daily statistics ...
+$SCRIPTS_DIR/./daily_profile_stats.sh
+
+# Generate am model files and compute spectra.
+export OUTDIR_AM=${SITE_DIR}/daily_am_models
+if [ ! -d $OUTDIR_AM ]; then
+    mkdir $OUTDIR_AM
+fi
+echo generating am model files ...
+$SCRIPTS_DIR/./daily_make_am_models.sh
+echo computing am models ...
+$SCRIPTS_DIR/./daily_run_am_models.sh
+
+# Some housekeeping
+# rm -r $OUTDIR_AM/am_cache
+# echo cleaning up ...
+# python3 $SCRIPTS_DIR/cleanup.py ${SITE_DIR}
+
+# Tabulate the results
+# source $VENV/bin/activate
+# python3 $SCRIPTS_DIR/tabulate.py ${SITE_DIR} ${SITE_LABEL}
+# deactivate
+
+echo done. Outputs written to $OUTDIR_PROFILES, $OUTDIR_AM.
